c25eaabc49af6b985049caae9f326d4ab5a10fc3
pratik-dutta <pratikdutta.786@gmail.com>
2020-04-12 15:25:18 +0530

    add que 2 again

--- Q2.js
+++ Q2.js
@@ -1,14 +1,32 @@
-//const obj = {
-//	a: 1,
-//	b: 2,
-//	c: {
-//		d: 100,
-//		e: {
-//			f: 45
-//		}
-//	}
-//};
-//console.log(propertyExists(obj, ‘c.d’)); // should output 100
-//console.log(propertyExists(obj, ‘c.d.g.h’)); // should output false
+// Q2. Implement a function propertyExists(obj, path) that takes in an object and a path (string) as arguments 
+//and returns ‘False’ if the property doesn’t exist on that object or is null, else returns the value of the property
 
-console.log("Hi");
+const obj = {
+	a: 1,
+	b: 2,
+	c: {
+		d: 100,
+		e: {
+			f: 45
+		}
+	}
+};
+
+Object.prototype.propertyExists = function(obj, k){
+	var properties = k.split('.');
+
+    for (var i = 0; i < properties.length; i++) {
+        var prop = properties[i];
+
+        if(!obj || !obj.hasOwnProperty(prop)){
+            return false;
+        } else {
+            obj = obj[prop];
+        }
+    }
+
+    return true;
+}
+
+console.log(propertyExists(obj, "c.d"));
+console.log(propertyExists(obj, "c.d.g.h"));
